afa92316230c116461924ebff8a5c489055f676c
Tello <95242465+Tellotello@users.noreply.github.com>
2023-09-03 01:37:33 -0600

    Update server.py

--- Reto/server.py
+++ Reto/server.py
@@ -1,11 +1,12 @@
-import mesa
-
 from model_bien import Estante, BandaEntrada, BandaSalida, EstacionDeCarga, RobotDeCarga, Paquete
 from model_bien import Habitacion
+import mesa.visualization
+import time
+import requests
+
 
 MAX_NUMBER_ROBOTS = 20
-
-## reto
+M = N = 15
 
 def agent_portrayal(agent):
     if isinstance(agent, Estante):
@@ -21,8 +22,7 @@ def agent_portrayal(agent):
         portrayal["Color"] = "#ccbeaf"
         portrayal["text"] = "📦"
         return portrayal
-        
-    
+
 grid = mesa.visualization.CanvasGrid(
     agent_portrayal, 15, 15, 300, 300)
 
@@ -51,77 +51,55 @@ model_params = {
     "N": 15,
 }
 
-server = mesa.visualization.ModularServer(
+# Initialize the Mesa server
+def run_mesa_server(num_robots):
+
+    if num_robots is None:
+        num_robots = 2  # Default value
+
+    model_params = {
+    "num_agentes": mesa.visualization.Slider(
+        "Número de Robots",
+        num_robots,
+        2,
+        MAX_NUMBER_ROBOTS,
+        1,
+        description="Escoge cuántos robots deseas implementar en el modelo",
+    ),
+    "modo_pos_inicial": mesa.visualization.Choice(
+        "Posición Inicial de los Robots",
+        "Aleatoria",
+        ["Fija", "Aleatoria"],
+        "Seleciona la forma se posicionan los robots"
+    ),
+    "M": 15,
+    "N": 15,
+}
+    server = mesa.visualization.ModularServer(
     Habitacion, [grid, chart_celdas],
     "botCleaner", model_params, 8521
 )
+   
+    server.launch(open_browser=True)
 
-## reto
+num_robots = None
+while num_robots is None:
+    print("Waiting for the number of robots to be set...")
+    
+    try:
+        # Make a GET request to the API
+        response = requests.get('http://127.0.0.1:5000/api/params')
+        if response.status_code == 200:
+            data = response.json()
+            num_robots = data.get("num_robots", None)
+            
+            if num_robots is not None:
+                print(f"Number of robots set to {num_robots}")
+                break  # Exit the loop
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
-# def agent_portrayal(agent):
-#     if isinstance(agent, RobotLimpieza):
-#         return {"Shape": "circle", "Filled": "false", "Color": "Cyan", "Layer": 1, "r": 0.9,
-#                 "text": f"{agent.carga}", "text_color": "black"}
-#     elif isinstance(agent, Mueble):
-#         return {"Shape": "rect", "Filled": "true", "Color": "black", "Layer": 0,
-#                 "w": 0.9, "h": 0.9}
-#     elif isinstance(agent, EstacionDeCarga):
-#         return {"Shape": "rect", "Filled": "true", "Layer": 0, "Color": "gray", "w": 0.9, "h": 0.9, "text": "⚡", "text_color": "Black"}
-#     elif isinstance(agent, Celda):
-#         portrayal = {"Shape": "rect", "Filled": "true", "Layer": 0, "w": 0.9, "h": 0.9, "text_color": "Black"}
-#         if agent.sucia:
-#             portrayal["Color"] = "#ccbeaf"
-#             portrayal["text"] = "💩"
-#         else:
-#             portrayal["Color"] = "white"
-#             portrayal["text"] = ""
-#         return portrayal
+    time.sleep(1)  # Wait for 1 second
 
-
-# grid = mesa.visualization.CanvasGrid(
-#     agent_portrayal, 15, 15, 300, 300)
-# chart_celdas = mesa.visualization.ChartModule(
-#     [{"Label": "CeldasSucias", "Color": '#36A2EB', "label": "Celdas Sucias"}],
-#     50, 200,
-#     data_collector_name="datacollector"
-# )
-
-# model_params = {
-#     "num_agentes": mesa.visualization.Slider(
-#         "Número de Robots",
-#         5,
-#         2,
-#         MAX_NUMBER_ROBOTS,
-#         1,
-#         description="Escoge cuántos robots deseas implementar en el modelo",
-#     ),
-#     "porc_celdas_sucias": mesa.visualization.Slider(
-#         "Porcentaje de Celdas Sucias",
-#         0.3,
-#         0.0,
-#         0.75,
-#         0.05,
-#         description="Selecciona el porcentaje de celdas sucias",
-#     ),
-#     "porc_muebles": mesa.visualization.Slider(
-#         "Porcentaje de Muebles",
-#         0.1,
-#         0.0,
-#         0.25,
-#         0.01,
-#         description="Selecciona el porcentaje de muebles",
-#     ),
-#     "modo_pos_inicial": mesa.visualization.Choice(
-#         "Posición Inicial de los Robots",
-#         "Aleatoria",
-#         ["Fija", "Aleatoria"],
-#         "Seleciona la forma se posicionan los robots"
-#     ),
-#     "M": 15,
-#     "N": 15,
-# }
-
-# server = mesa.visualization.ModularServer(
-#     Habitacion, [grid, chart_celdas],
-#     "botCleaner", model_params, 8521
-# )
+run_mesa_server(num_robots)
